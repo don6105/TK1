0128e73282bb47a98cdeb4f42234878560ac9d7a
don6105 <don6105@users.noreply.github.com>
2015-12-07 13:07:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@
 
 # check root permission
 if [ ${EUID} -ne 0 ]; then
-   echo "This script must be run as root" 
-   exit 1
+	echo "This script must be run as root" 
+	exit 1
 fi
 
 # check L4T version
